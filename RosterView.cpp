35c142024e4a662779b638bc4dc90e1a981c0282
lemon <email>
2010-08-23 01:26:22 +0000

    git-svn-id: svn://svn.berlios.de/dengon/trunk@119 c14d4bca-7b17-0410-87b9-834895956487

--- RosterView.cpp
+++ RosterView.cpp
@@ -315,7 +315,7 @@ void RosterView::UpdateRoster()
 			{
 				item->SetStalePointer(true);
 				RemoveItem(item);
-				fprintf(stderr, "RostetView::UpdateRoster process removomals");
+				//fprintf(stderr, "RostetView::UpdateRoster process removomals");
 				goto RESET;
 			}
 			
