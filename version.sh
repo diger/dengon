99d571265d60a1813e80b1ec653feb855241f326
lemon <email>
2010-08-26 07:24:41 +0000

    git-svn-id: svn://svn.berlios.de/dengon/trunk@145 c14d4bca-7b17-0410-87b9-834895956487

--- version.sh
+++ version.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 svnversion=`LC_ALL=C svn info | awk '/^Revision:/ {print $2}'`
-svndate=`LC_ALL=C svn info | awk '/^Last Changed Date:/ {print $4,$5}'`
+svndate=`LC_ALL=C svn info | awk '/^Last Changed Date:/ {print $4}'`
 gendate=`LC_ALL=C date`
 cat <<EOF > version.h
 
