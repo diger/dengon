582ead8b4d0b46c8afce12128ce52f11262bf2d4
lemon <email>
2010-08-29 16:13:32 +0000

    git-svn-id: svn://svn.berlios.de/dengon/trunk@180 c14d4bca-7b17-0410-87b9-834895956487

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 
 // Do not edit!
 //
-// generated from ./version.sh on Sun Aug 29 17:43:19 GMT 2010
+// generated from ./version.sh on Sun Aug 29 19:12:42 GMT 2010
 
-#define DENGON_SVNVERSION "177"
+#define DENGON_SVNVERSION "179"
 #define DENGON_SVNDATE "2010-08-29"
 
